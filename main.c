828f7f6bf1e481116d9542afe4f965283af286f2
Jacek Borkowski <jacek.borkowskiit@gmail.com>
2025-03-30 21:02:03 +0200

    timer to display segments

--- main.c
+++ main.c
@@ -15,15 +15,13 @@ const uint8_t digitTable[10] = {
     0b01101111  // 9
 };
 
-// Digit control pins (adjust based on your wiring)
 #define DIGIT1_EN (1 << PB0)
 #define DIGIT2_EN (1 << PB1)
 #define DIGIT3_EN (1 << PB2)
 #define DIGIT4_EN (1 << PB3)
 
-// Array to hold the digits to display
-uint8_t displayDigits[4] = {1, 2, 3, 4}; // Start with 1, 2, 3, 4
-volatile uint8_t currentDigit = 0;
+volatile uint16_t value = 0;
+volatile uint8_t display_number[4] = {0, 0, 0, 0};
 
 void display_digit(uint8_t digit, uint8_t value) {
   PORTB = ~digit; // Turn ON the specified digit (assuming common cathode)
@@ -31,12 +29,18 @@ void display_digit(uint8_t digit, uint8_t value) {
   _delay_ms(1);
 }
 
-ISR(TIMER1_COMPA_vect) { currentDigit = (currentDigit + 1) % 10; }
+ISR(TIMER1_COMPA_vect) {
+  value = (value + 1) % 10000;
+  display_number[0] = value % 10;
+  display_number[1] = (value / 10) % 10;
+  display_number[2] = (value / 100) % 10;
+  display_number[3] = value / 1000;
+}
 
 void timer1_init() {
-  TCCR1B |= (1 << WGM12); // Tryb CTC
-  OCR1A = 15624;          // Przerwanie co 1 sekundę (dla 8 MHz, prescaler 1024)
-  TIMSK |= (1 << OCIE1A); // Włącz przerwanie Timer1 CTC
+  TCCR1B |= (1 << WGM12);              // Configure timer1 for CTC mode
+  OCR1A = 1562;                        // count till 1562 to get 1 second delay
+  TIMSK |= (1 << OCIE1A);              // Enable Timer1 compare interrupt
   TCCR1B |= (1 << CS12) | (1 << CS10); // Prescaler 1024
 }
 
@@ -50,11 +54,9 @@ int main(void) {
   uint8_t counter = 0;
 
   while (1) {
-    PORTB = 0x0F; // PB0-PB3 as outputs (digit enable)
-    _delay_ms(1);
-    display_digit(DIGIT1_EN, currentDigit);
-    display_digit(DIGIT2_EN, (currentDigit + 1) % 10);
-    display_digit(DIGIT3_EN, (currentDigit + 2) % 10);
-    display_digit(DIGIT4_EN, (currentDigit + 3) % 10);
+    display_digit(DIGIT1_EN, display_number[0]);
+    display_digit(DIGIT2_EN, display_number[1]);
+    display_digit(DIGIT3_EN, display_number[2]);
+    display_digit(DIGIT4_EN, display_number[3]);
   }
 }
